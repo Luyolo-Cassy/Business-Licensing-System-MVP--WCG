06d0a60b042430f420b52b62bda8bca9dd67bcfe
AlecCabral <cbrale002@myuct.ac.za>
2026-05-13 11:41:16 +0200

    Saved remaining changes

--- obj/Debug/net10.0/BusinessLicensing Practice.AssemblyInfo.cs
+++ obj/Debug/net10.0/BusinessLicensing Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLicensing_Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7f40e4911c67b4beecd4ef5619f5ea89119674")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLicensing_Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLicensing_Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
