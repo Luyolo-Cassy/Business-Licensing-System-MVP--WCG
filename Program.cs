b593ffdcfe55554a8d836244b2f326cf1db6f32e
Luyolo Madasi <mdsluy002@myuct.ac.za>
2026-07-14 14:23:38 +0200

    Login round one

--- Program.cs
+++ Program.cs
@@ -2,10 +2,20 @@ using BusinessLicensing_Practice.Components;
 using Microsoft.EntityFrameworkCore;
 using BusinessLicensing_Practice.Data;
 using BusinessLicensing_Practice.Models;
+using Microsoft.AspNetCore.Identity;
+using BusinessLicensing_Practice.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=businesslicensing.db"));
+    builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -22,6 +32,8 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAntiforgery();
 
@@ -34,6 +46,42 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
     db.Database.Migrate();
+
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+string[] roles = { "BusinessOwner", "MunicipalOfficial" };
+
+foreach (var role in roles)
+{
+    if (!await roleManager.RoleExistsAsync(role))
+    {
+        await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+
+// Create a default Municipal Official
+var officialEmail = "official@westerncape.gov.za";
+
+var official = await userManager.FindByEmailAsync(officialEmail);
+
+if (official == null)
+{
+    official = new ApplicationUser
+    {
+        UserName = officialEmail,
+        Email = officialEmail,
+        FullName = "Municipal Official"
+    };
+
+    var result = await userManager.CreateAsync(official, "Password123!");
+
+    if (result.Succeeded)
+    {
+        await userManager.AddToRoleAsync(official, "MunicipalOfficial");
+    }
+}
+    
 
     if (!db.Applications.Any())
     {
